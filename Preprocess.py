cc8bb30c977579b43dd5d3fbec7d0cdeea9c7f6b
Julia Chen <tc468@cornell.edu>
2018-06-13 18:48:42 -0700

    update train.py

--- Preprocess.py
+++ Preprocess.py
@@ -10,6 +10,7 @@ import numpy as np
 import os
 import glob
 import skimage.io as io
+#from keras import backend as keras
 #from skimage.color import rgb2gray
 
 # In[3]:
@@ -37,7 +38,7 @@ def adjustData(img, mask, flag_multi_class, num_class):
 
 def trainGenerator(batch_size, train_path, image_folder, mask_folder, aug_dict, image_color_mode = 'grayscale', 
                   mask_color_mode = 'grayscale', image_save_prefix = 'img', mask_save_prefix = 'mask',
-                  flag_multi_class = False, num_class = 1, save_to_dir = 'notebook/ex', target_size = (500, 500), seed = 1):
+                  flag_multi_class = False, num_class = 1, save_to_dir = None, target_size = (500, 500), seed = 1):
     image_datagen = ImageDataGenerator(**aug_dict)
     mask_datagen = ImageDataGenerator(**aug_dict)
     image_generator = image_datagen.flow_from_directory(
@@ -66,14 +67,18 @@ def trainGenerator(batch_size, train_path, image_folder, mask_folder, aug_dict, 
         img, mask = adjustData(img, mask, flag_multi_class, num_class)
         yield (img, mask)
 
+#def IOU_calc(y_true, y_pred):
+#   y_true_f = keras.flatten(y_true)
+#   y_pred_f = keras.flatten(y_pred)
+#   intersection = keras.sum(y_true_f * y_pred_f)
+    
+#   return 2*(intersection + smooth) / (keras.sum(y_true_f) + keras.sum(y_pred_f) + smooth)
 
 # In[5]:
 
 def testGenerator(test_path, num_image = 1, target_size = (500, 500), flag_multi_class = False, as_gray = True):
     #for i in range(num_image):
         img = io.imread(os.path.join(test_path, '0_01_01.png'), as_gray = as_gray)
-        #img = cv2.imread(os.path.join(test_path, '0_01_01.png'),0)
-        #img = rgb2gray(img) / 255
         img = img / 255
         #img = img.astype(np.float32)
         img = np.reshape(img, img.shape+(1,)) if (not flag_multi_class) else img
@@ -81,28 +86,10 @@ def testGenerator(test_path, num_image = 1, target_size = (500, 500), flag_multi
         yield img
 
 
-# In[6]:
-
-
-#def genTrainNpy(image_path, mask_path, flag_multi_class = False, num_class = 2, image_prefix = None, mask_prefix = None, image_as_gray = True, mask_as_gray = True):
-#image_name_arr = glob.glob(os.path.join(image_path, '%s*.png'%image_prefix))
-#image_arr = []
-# mask_arr = []
-# for index, item in enumerate(image_name_arr):
-#       img = io.imread(item, as_gray = image_as_gray)
-#       img = np.reshape(img, img.shape + (1,)) if image_as_gray else img
-#    mask = io.imread(item.replace(image_path, mask_path).replace(image_prefix, mask_prefix), as_gray = mask_as_gray)
-#       mask = np.reshape(mask, mask.shape + (1,)) if mask_as_gray else mask
-#       img, mask = adjustData(img, mask, flag_multi_class, num_class)
-#       image_arr.append(img)
-#       mask_arr.append(mask)
-#   image_arr = np.array(image_arr)
-#   mask_arr = np.array(mask_arr)
-#   return image_arr, mask_arr
-
-
-def saveResult(save_path, npyfile, flag_multi_class = False, num_class = 1):
+def saveResult(save_path, npyfile):
     for i, item in enumerate(npyfile):
-        img = item[:, :, 0]
-        io.imsave(os.path.join(save_path, '%d_predictgray256.png'%i), img)
+        img = item[:, :, -1]
+        print (img.shape)
+        print (img)
+        io.imsave(os.path.join(save_path, '%d_predictgray.png'%i), img)
 
--- trainUnet.py
+++ trainUnet.py
@@ -1,30 +1,61 @@
 
 # coding: utf-8
 
-# In[ ]:
-
-
 #get_ipython().run_line_magic('run', 'Unet_v1.ipynb')
 #get_ipython().run_line_magic('run', 'Preprocess.ipynb')
 from Unet_v2 import *
 from Preprocess import *
 import matplotlib.pyplot as plt
-# In[ ]:
+from Post import *
+import numpy as np
 
-
+## generate input image/label as a set
 data_gen_args = dict()
-myGene = trainGenerator(1, 'notebook', 'image', 'label', data_gen_args, save_to_dir = 'notebook/ex')
+myGene = trainGenerator(1, 'notebook', 'image', 'label', data_gen_args, save_to_dir = None)
+
+#check(myGene)
+
 model = unet()
-model_checkpoint = ModelCheckpoint('unet_gray.hdf5', monitor = 'loss', verbose = 1, save_best_only = True)
-model.fit_generator(myGene, steps_per_epoch = 2, epochs = 2, callbacks = [model_checkpoint])
+model_checkpoint = ModelCheckpoint('unet_.hdf5', monitor = 'loss', verbose = 1, save_best_only = True)
+history = model.fit_generator(myGene, steps_per_epoch = 10, epochs = 4, callbacks = [model_checkpoint, model_history])
 
-# In[ ]:
+hist_array = np.array(history.history['loss'])
+np.savetxt('loss_history.txt', hist_array, delimiter=',')
 
+# plot history
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+#plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+#plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
 
+# generate test image
 testGene = testGenerator('notebook/test')
 
 model = unet()
-model.load_weights('unet_gray.hdf5')
+model.load_weights('unet_.hdf5')
 results = model.predict_generator(testGene, 1, verbose = 1)
 saveResult('notebook', results)
 
+import matplotlib.image as pimg
+#import cv2 as cv
+
+img = pimg.imread('0_predictgray32.png')
+thre = 0.49
+
+binary = to_binary(img, 0.49)
+plt.imshow(binary, 'gray')
+plt.show()
+#io.imsave(os.path.join('notebook/binary', 'predictgray32.png'), binary)
