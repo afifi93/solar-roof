b742e90e12ef51f8e0056a217affe000744e8f6e
Julia Chen <tc468@cornell.edu>
2018-06-15 08:32:15 -0700

    upload smaller unet version

---
+++ Post.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+
+# Post processing prediction image to binary image
+def to_binary(pred, th):
+    pred[pred > th] = 1
+    pred[pred <= th] = 0
+    print (pred)
+    return pred
+
+# draw the boundary
+def draw_bound(img):
+#    img = cv2.imread('path/test.jpg')
+    imbw = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    (thresh, im) = cv2.threshold(imbw, 0.5, 1, 0)
+    _, contours,_ = cv2.findContours(im, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(img,contours,-1,(0,255,0),3)
+    cv2.imwrite('cnt.png', img)
+
