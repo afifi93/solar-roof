45f7aab07b5f5a3cdefad6e52cd3c670648f9b6b
Julia Chen <tc468@cornell.edu>
2018-06-21 11:29:16 -0700

    update post.py

--- post.py
+++ post.py
@@ -1,10 +1,14 @@
 # Functions for post-processing steps
-#import cv2
 import os
 import skimage.io as io
-#import requests
 import numpy as np
 import cv2
+import glob
+from PIL import Image
+import requests
+import urllib
+import matplotlib.pyplot as plt
+import json
 
 # Save the prediction image at the provided save_path
 def saveResult(save_path, npyfile):
@@ -12,41 +16,58 @@ def saveResult(save_path, npyfile):
         img = item[:, :, 0]
         io.imsave(os.path.join(save_path, '%d_pred.png'%i), img)
 
-## Post-processing prediction image to binary image
-#def to_binary(pred, th):
-#    pred[pred > th] = 1
-#    pred[pred <= th] = 0
-#    return pred
-#
 ## Draw the boundary; prediction results as input; resize back to original size
-#def draw_bound(npyfile, test_path, ori_size):
-#    for i, item in enumerate(npyfile):
-#        test = io.imread(os.path.join(test_path,"%d.png"%i))
-#img = io.imread('0_pred.png')
-##img = item[:, :, 0]
-#imbw = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-#gray_blur = cv2.GaussianBlur(imbw, (15, 15), 0)
-#thresh = cv2.adaptiveThreshold(gray_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 1)
-#kernel = np.ones((3, 3), np.uint8)
-#closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=4)
-#cont_img = closing.copy()
-#contours, hierarchy = cv2.findContours(cont_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#for cnt in contours:
-#    area = cv2.contourArea(cnt)
-#    if area < 30:
-#        continue
-#cv2.drawContours(img,cnt,-1,(0,255,0),2)
-#cv2.imwrite('0_ct.png',img)
-#cv2.imshow('final result', roi)
+def draw_boundary(pred_path, test_path, ori_size):
+    pred_list = sorted(glob.glob('*_pred.png'))
+    test_list = sorted(glob.glob('*_test.png'))
+    print (pred_list)
+    print (test_list)
+    for i in range (len(pred_list)):
+#        if os.path.isfile(os.path.join(pred_path,'%d_pred.png'%i)):
+#            img = cv2.imread(os.path.join(pred_path,'%d_pred.png'%i))
+            img = cv2.imread(os.path.join(pred_path, pred_list[i]))
+#            test = Image.open(os.path.join(test_path,'%d_test.png'%i))
+            test = Image.open(os.path.join(test_path, test_list[i]))
+#        else:
+#            raise ValueError('Path provided is not a valid file: {}'.format(pred_path))
+            test_resize = test.resize(ori_size, Image.NEAREST)
+            test_arr = np.array(test_resize)
+            imbw = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            (thresh, im) = cv2.threshold(imbw, 128, 255, 0)
+            _, contours, _ = cv2.findContours(im, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            val_cnt = []
+            for cnt in contours:
+                area = cv2.contourArea(cnt)
+                print (area)
+                if area > 400:
+                    val_cnt.append(cnt)
+            print (len(val_cnt))
+            cv2.drawContours(test_arr,val_cnt,-1,(0,255,0),3)
+            cv2.imwrite('%d_ct.png'%i,test_arr)
 
-#        (thresh, im) = cv2.threshold(imbw, 0.5, 1, 0)
-#        _, contours,_ = cv2.findContours(im, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-#        cnt = trans.resize(contours,ori_size)
-#        cv2.drawContours(img,cnt,-1,(0,255,0),2)
-#        cv2.imwrite('%d_cnt.png'%i, test)
+#draw_boundary('', '', (256,256))
 
-# Grab roof area from API
-#r = requests('https://www.google.com/async/sclp?async=lat:{},lng:{}'.format{lat, lng})
-#np.savetxt('API query.txt', r.text())
-# extract "roof_good_solar_square_feet":
+# Grab roof image from googlemap API
+center_x = 37.451736
+center_y = -122.146029
+r = requests.get('https://maps.googleapis.com/maps/api/staticmap?center={},{} &zoom=19&size=200x200&maptype=satellite&'.format(center_x, center_y))
+# url = "https://maps.googleapis.com/maps/api/staticmap?center={},{} &zoom=19&size=200x200&maptype=satellite&'.format(center_x, center_y)"
+# url_response = urllib.urlopen()
+img_array = np.array(bytearray(r.content), dtype=np.uint8)
+img = cv2.imdecode(img_array, -1)
+print (img.shape)
+#cv2.imshow('URL Image', img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+plt.imshow(img)
+plt.show()
+
+# request roof area from sunroof API
+url = 'https://www.google.com/async/sclp?async=lat:{},lng:{}'.format(center_x, center_y)
+
+response = requests.get(url)
+r = (response.text[4:])
+d = json.loads(r)
+
+print (d["HouseInfoResponse"]["house_assumptions"]['roof_good_solar_square_feet'])
 
